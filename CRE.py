33b86d0fe7d63dcd7c8efae5126bdcd1c51a651b
owamsley <Mickey20122012>
2019-03-25 21:55:24 -0600

    -ready for help with changing binary to the different value types. -blinking wasn't able to be implemented because it killed the  other code for some reason. It's still there just commented -was.pressed() functions don't work in some cases, so I have  been using is.pressed() even though if the other function was  working it'd work better than is.pressed()

--- CRE.py
+++ CRE.py
@@ -1,34 +1,109 @@
-# binaryString = [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 from microbit import *
+import time
 
-binaryString = []
-
-for x in range(26):
-    x_axis = x % 5
-    y_axis = x / 5
-    # bit 0 is at (0,0)
-    changeBit(x, x_axis, y_axis, binaryString)
-
-microbit.display.clear()
-
-for x in range(26, 33):
-    y = x
-    y = y - 26
+screen = 0
+binaryString = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+bitPatternScreen1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+bitPatternScreen2 = [0, 0, 0, 0, 0, 0, 0]
+stage = 0
 
 
-def changeBit(x, x_axis, y_axis, binaryString):
-    microbit.display.set_pixel(x_axis, y_axis, 0)
-    z = 0
-    while true:
+# stage 0 is selecting bits, 1 is other stuff
 
-        if button_a.is_pressed() and z == 0:
-            microbit.display.set_pixel(x_axis, y_axis, 0)
-            z = 0
+def displayBit():
+    display.scroll("good")
+
+def changeBit(x, x_axis, y_axis, binaryString, screen):
+    z = binaryString[x]
+    if screen == 0:
+        bitPatternValue = bitPatternScreen1[x]
+    if screen == 1:
+        bitPatternValue = bitPatternScreen2[(x-25)]
+    display.set_pixel(x_axis, y_axis, bitPatternValue)
+    while True:
+        #display.set_pixel(x_axis, y_axis, bitPatternValue)
+        #sleep(250)
+        #display.set_pixel(x_axis, y_axis, 5)
+        #sleep(250)
+        # was_pressed() doesn't work for some reason here, and blinker lines break the code?
         if button_a.is_pressed() and z == 1:
-            microbit.display.set_pixel(x_axis, y_axis, 9)
+            display.set_pixel(x_axis, y_axis, 0)
+            z = 0
+            bitPatternValue = 0
+            sleep(150)
+        if button_a.is_pressed() and z == 0:
+            display.set_pixel(x_axis, y_axis, 9)
             z = 1
-        if button_b.is_pressed():
+            bitPatternValue = 9
+            sleep(150)
+        if button_b.was_pressed():
             break
-    binaryString.append(z)
+    if screen == 0:
+        bitPatternScreen1[x] = bitPatternValue
+        binaryString[x] = z
+    if screen == 1:
+        # first value in this if nest will be x = 25, this is right for the binaryString
+        bitPatternScreen2[(x-25)] = bitPatternValue
+        binaryString[x] = z
 
+def changeSequence():
+    bps1 = [str(c) for c in bitPatternScreen1]
+    bps1i = ''.join(bps1[0:5]) + ":" + ''.join(bps1[5:10]) + ":" + ''.join(bps1[10:15]) + ":" + ''.join(
+        bps1[15:20]) + ":" + ''.join(bps1[20:25])
+    screen1 = Image(bps1i)
+    display.show(screen1)
+    screen = 0
 
+    for x in range(25):
+        x_axis = x % 5
+        y_axis = x // 5
+        # bit 0 is at (0,0)
+        changeBit(x, x_axis, y_axis, binaryString, screen)
+        if button_b.is_pressed():
+            time_start = running_time()
+            while button_b.is_pressed():
+                sleep(5)
+            time_stop = running_time()
+        b_length = time_stop - time_start
+        if b_length > 500:
+            displayBit()
+
+    screen = (screen + 1) % 2
+    bps2 = [str(d) for d in bitPatternScreen2]
+    bps2i = ''.join(bps2[0:5]) + ":" + ''.join(bps2[5:10]) + ":" + ''.join(bps2[10:15]) + ":" + ''.join(
+        bps2[15:20]) + ":" + ''.join(bps2[20:25])
+    screen2 = Image(bps2i)
+    display.show(screen2)
+
+    for x in range(25, 32):
+        y = x
+        y = y - 25
+        x_axis = y % 5
+        y_axis = y // 5
+        changeBit(x, x_axis, y_axis, binaryString, screen)
+        if button_b.is_pressed():
+            time_start = running_time()
+            while button_b.is_pressed():
+                sleep(5)
+            time_stop = running_time()
+        b_length = time_stop - time_start
+        if b_length > 500:
+            displayBit()
+
+while True:
+    changeSequence()
+
+# for x in range(25):
+#    x_axis = x % 5
+#   y_axis = x // 5
+# bit 0 is at (0,0)
+#  changeBit(x, x_axis, y_axis, binaryString)
+
+# display.clear()
+
+# for x in range(26, 33):
+#   y = x
+#  y = y - 26
+# x_axis = y % 5
+# y_axis = y //5
+# changeBit(x, x_axis, y_axis, binaryString)
