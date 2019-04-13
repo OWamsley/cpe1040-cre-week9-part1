00b44be8cb62f77606111e1216414509202a415f
owamsley <Mickey20122012>
2019-04-13 13:20:46 -0600

    -all done -Things I didn't implement because I couldn't figure it out: blinking cursor, ascii conversion

--- CRE.py
+++ CRE.py
@@ -1,6 +1,9 @@
 from microbit import *
 import time
 
+from microbit import *
+import time
+
 screen = 0
 binaryString = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 bitPatternScreen1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -8,19 +11,85 @@ bitPatternScreen2 = [0, 0, 0, 0, 0, 0, 0]
 stage = 0
 
 
-# stage 0 is selecting bits, 1 is other stuff
-
-def displayBit():
-    display.scroll("good")
-
-
 def binary_to_unsigned():
     unsigned = 0
     y = 0
     for i in range(31, -1, -1):
-        unsigned += (binaryString[i] * (2**y))
+        unsigned += (binaryString[i] * (2 ** y))
         y = y + 1
-    print(unsigned)
+    return unsigned
+
+
+def binary_to_signed():
+    global binaryString
+
+    signed = 0
+    y = 0
+    print(binaryString)
+    if binaryString[0] == 1:
+        binaryStringInverse = []
+        # flip the bits
+        for i in binaryString:
+            # binaryStringInverse.append(binaryString[i])
+            if i == 0:
+                binaryStringInverse.append(1)
+            else:
+                binaryStringInverse.append(0)
+        for i in range(31, 0, -1):
+            signed += (binaryStringInverse[i] * (2 ** y))
+            y = y + 1
+        signed = signed + 1
+        signed = signed * -1
+    else:
+        for i in range(31, 0, -1):
+            signed += (binaryString[i] * (2 ** y))
+            y = y + 1
+    return signed
+
+
+def binary_to_float():
+    sign = 1
+    exponent = 0
+    mantissa = 0
+    y = 0
+    if binaryString[0] == 1:
+        sign = -1
+    for i in range(9, 0, -1):
+        exponent += (binaryString[i] * (2 ** y))
+        y += 1
+    exponent = exponent - 127
+    y = 1
+    for i in range(9, 32, ):
+        mantissa += (binaryString[i] * (2 ** -y))
+        y += 1
+    mantissa = mantissa + 1
+    float_value = sign * (2 ** exponent) * mantissa
+    return float_value
+
+
+# stage 0 is selecting bits, 1 is other stuff
+
+def displayBit():
+    unsigneddisplay = binary_to_unsigned()
+    signeddisplay = binary_to_signed()
+    floatdisplay = binary_to_float()
+    while True:
+        while True:
+            display.scroll('U: ', delay=150, wait=True)
+            display.scroll(unsigneddisplay, delay=150, wait=True, loop=False, monospace=False)
+            if button_a.was_pressed():
+                break
+
+        while True:
+            display.scroll('S: ', delay=150, wait=True)
+            display.scroll(signeddisplay, delay=150, wait=True, loop=False, monospace=False)
+            if button_a.was_pressed():
+                break
+        while True:
+            display.scroll('F: ', delay=150, wait=True)
+            display.scroll(floatdisplay, delay=150, wait=True, loop=False, monospace=False)
+            if button_a.was_pressed():
+                break
 
 
 def changeBit(x, x_axis, y_axis, binaryString, screen):
@@ -28,24 +97,24 @@ def changeBit(x, x_axis, y_axis, binaryString, screen):
     if screen == 0:
         bitPatternValue = bitPatternScreen1[x]
     if screen == 1:
-        bitPatternValue = bitPatternScreen2[(x-25)]
+        bitPatternValue = bitPatternScreen2[(x - 25)]
     display.set_pixel(x_axis, y_axis, bitPatternValue)
     while True:
-        #display.set_pixel(x_axis, y_axis, bitPatternValue)
-        #sleep(250)
-        #display.set_pixel(x_axis, y_axis, 5)
-        #sleep(250)
+        # display.set_pixel(x_axis, y_axis, bitPatternValue)
+        # sleep(250)
+        # display.set_pixel(x_axis, y_axis, 5)
+        # sleep(250)
         # was_pressed() doesn't work for some reason here, and blinker lines break the code?
         if button_a.is_pressed() and z == 1:
             display.set_pixel(x_axis, y_axis, 0)
             z = 0
             bitPatternValue = 0
-            sleep(150)
+            sleep(250)
         if button_a.is_pressed() and z == 0:
             display.set_pixel(x_axis, y_axis, 9)
             z = 1
             bitPatternValue = 9
-            sleep(150)
+            sleep(250)
         if button_b.was_pressed():
             break
     if screen == 0:
@@ -53,8 +122,9 @@ def changeBit(x, x_axis, y_axis, binaryString, screen):
         binaryString[x] = z
     if screen == 1:
         # first value in this if nest will be x = 25, this is right for the binaryString
-        bitPatternScreen2[(x-25)] = bitPatternValue
+        bitPatternScreen2[(x - 25)] = bitPatternValue
         binaryString[x] = z
+
 
 def changeSequence():
     bps1 = [str(c) for c in bitPatternScreen1]
@@ -99,6 +169,7 @@ def changeSequence():
         b_length = time_stop - time_start
         if b_length > 500:
             displayBit()
+
 
 while True:
     changeSequence()
